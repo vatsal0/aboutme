810bc000fb81888a342ac0a70df2ce15a6563e9f
Vatsal Baherwani <vatsalbaherwani@gmail.com>
2021-07-24 13:54:08 -0400

    Page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'aboutme',
+    title: 'Vatsal Baherwani',
     htmlAttrs: {
       lang: 'en'
     },
